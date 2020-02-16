be5ecf5e8d83dd6465a02eb32143f3b20cec514b
Duc Do <ddo@beyond.ai>
2020-02-16 11:30:43 -0800

    added boilerplate ML pipeline functions

---
+++ {{cookiecutter.repo_name}}/src/config/config.py
@@ -0,0 +1,3 @@
+import pathlib
+
+DATA_DIR = None
---
+++ {{cookiecutter.repo_name}}/src/modeling/pipeline.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+from sklearn import ensemble
+from sklearn import preprocessing
+from sklearn import metrics
+import joblib
+
+
+def load_dataset():
+    # Load dataset
+    pass
+
+
+def build_model():
+    # Data prep and fit vanilla model
+    pass
+
+
+def tune_model():
+    # Retrieve a vanilla model from build_model(), then tune its hyperparameters
+    pass
+
+
+def train_model():
+    # Retrieve a vanilla model from build_model(), retrieve its hyperparameters, then fit it  
+    pass
+
+def validate_model(validation_type='cross_validation'):
+    # perform validation, default to be cross_validation
+    pass
+    
+
